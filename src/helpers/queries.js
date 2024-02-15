e48e32555a2f9ed2b3f183a4bce99d4a7e5420b1
Cacho Medina <zanlorenzocacho10@gmail.com>
2024-02-15 11:06:23 -0300

    funcion crear

--- src/helpers/queries.js
+++ src/helpers/queries.js
@@ -2,4 +2,18 @@ const URI_Producto = import.meta.env.VITE_API_PRODUCTO;
 
 console.log(URI_Producto);
 
-const crear = () => {};
+const crear = async (productoNuevo) => {
+    try {
+        const res = await fetch(URI_Producto, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(productoNuevo),
+        });
+        console.log(res);
+        return res;
+    } catch (error) {
+        console.log(error);
+    }
+};
